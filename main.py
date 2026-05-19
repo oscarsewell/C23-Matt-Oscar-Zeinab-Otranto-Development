510321351084874766adf6f4147932cd8b20f9ea
zeihu <zeinabhussaintech@gmail.com>
2026-05-19 10:59:05 +0100

    testing workflows

---
+++ main.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+def checking_workflows():
+    print("hello world")
+
+
+def double_checking():
+    print("hello world again")
+
+
+if __name__ == "__main__":
+    checking_workflows()
